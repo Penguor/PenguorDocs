d1eb5ab76cdc0e5a1777b23f80934d3e94625c2e
Carl Schierig <coozypenguin@gmail.com>
2021-04-21 19:22:16 +0200

    update pygments lexer

--- PenguorLexer/penguorlexer/lexer.py
+++ PenguorLexer/penguorlexer/lexer.py
@@ -13,7 +13,20 @@ class PenguorLexer(RegexLexer):
             (r'(?<=system)\s+[A-Za-z_]\w*', Name.Class),
             (r'(?<=data)\s+[A-Za-z_]\w*', Name.Class),
             (r'(?<=type)\s+[A-Za-z_]\w*', Name.Class),
+            
+            # comments
             (r'//.*?$', Comment.Single),
             
+            # numbers
+            (r'(?<![A-Za-z])(\d*\.\d+|\d+\.\d+)', Number.Float),
+            (r'(?<![A-Za-z\.\d])\d+(?!\.)', Number.Integer),
+            (r'0h[0-9A-Fa-f]+', Number.Hex),
+            (r'0b[01]+', Number.Bin),
+            (r'0o[0-7]+', Number.Bin),
+            (r'0x[0-9A-Za-z]+', Number.Bin),
+            
+            # strings
+            (r"'.'", String.Char),
+            (r'".*"', String.Double),
         ]
     }
