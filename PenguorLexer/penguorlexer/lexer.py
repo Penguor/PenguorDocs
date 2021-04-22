ccf3a2229505ce009e8b5d2be49915cf9503e500
Carl Schierig <coozypenguin@gmail.com>
2021-04-22 18:28:27 +0200

    update theme and more work on lexer

--- PenguorLexer/penguorlexer/lexer.py
+++ PenguorLexer/penguorlexer/lexer.py
@@ -9,24 +9,33 @@ class PenguorLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'[A-Za-z_]\w*(?=\()', Name.Function),
+            (r'[A-Za-z_]\w*\s*(?=\()', Name.Function),
             (r'(?<=system)\s+[A-Za-z_]\w*', Name.Class),
             (r'(?<=data)\s+[A-Za-z_]\w*', Name.Class),
             (r'(?<=type)\s+[A-Za-z_]\w*', Name.Class),
             
             # comments
             (r'//.*?$', Comment.Single),
+            (r'/\*.*\*/', Comment.Multiline),
             
             # numbers
             (r'(?<![A-Za-z])(\d*\.\d+|\d+\.\d+)', Number.Float),
             (r'(?<![A-Za-z\.\d])\d+(?!\.)', Number.Integer),
             (r'0h[0-9A-Fa-f]+', Number.Hex),
             (r'0b[01]+', Number.Bin),
-            (r'0o[0-7]+', Number.Bin),
-            (r'0x[0-9A-Za-z]+', Number.Bin),
+            (r'0o[0-7]+', Number.Oct),
+            (r'0x[0-9A-Za-z]+', Number),
             
             # strings
             (r"'.'", String.Char),
             (r'".*"', String.Double),
+            
+            #keywords
+            
+            #builtin types
+            (r'(i8|i16|i32|i64|u8|u16|u32|u64|f32|f64|char|str|bool|void)', Keyword.Type),
+            
+            (r'(true|false|null)', Keyword.Constant),
+            (r'(return|if|else|while|do|for|switch|case|default)', Keyword.Reserved)
         ]
     }
